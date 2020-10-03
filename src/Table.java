38cdae4fce517a6e82dd3cc3102da3448ee4f92b
Andrew Gallup <agallup@Andrews-MBP.hsd1.ma.comcast.net>
2020-10-03 18:05:21 -0400

    Added a bunch of stuff

--- src/Table.java
+++ src/Table.java
@@ -27,7 +27,7 @@ public class Table {
     private int running_count;
     private List<Integer> deck;
     private List<Integer> dealer_hand;
-    private List<Integer> player_hand;
+    private List<ArrayList<Integer>> player_hands;
 
     public Table(int table_min, int spread, int hands_per_hr, int shoe_size, double penetration, boolean hit_on_soft, double original_bankroll) {
         this.table_min = table_min;
@@ -42,7 +42,10 @@ public class Table {
         this.running_count = 0;
         this.deck = new ArrayList<Integer>();
         this.dealer_hand = new ArrayList<Integer>();
-        this.player_hand = new ArrayList<Integer>();
+        this.player_hands = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < 4; i++){
+            player_hands.add(new ArrayList<Integer>());
+        }
 
         shuffle();
     }
@@ -78,15 +81,190 @@ public class Table {
     }
 
     /**
+     * Determines if the deck needs to be shuffled based on deck penetration
+     * @return
+     */
+    public boolean deck_needs_shuffle(){
+        //if deck_penetration percentage of cards have been dealt, return true, else false
+        return (deck.size()/(52*shoe_size) + deck_penetration <= 1);
+    }
+
+    /**
     * Calculates true count based on running count and decks_remaining
+     *
+     * Args:
+     *  @param running (int) : Running Count (usually should be running_count)
+     *  @param deck_size (int): number of cards remaining in deck (usually should be deck.size())
+     *
+     * Returns:
+     *  True count (double)
     */
     public double calculate_true_count(){
-        return running_count/(deck.size()/52.0);
+        return double(running_count)/(double(deck.size())/52.0);
+    }
+
+    /**
+     * Adds money from the bankroll to the pot based on the true count and the
+     */
+    public void make_bet(){
+    }
+
+    /**
+     * Takes cards from the deck and moves them to the player/dealer hands.
+     * increments the count based on cards dealt
+     */
+    public void deal_cards(){
+        //clear the hands
+        for (int i = 0; i < 4; i++){
+            player_hands.get(i).clear();
+        }
+        dealer_hand.clear();
+
+        //pop 4 cards off of deck, 2 to each hand
+        int first_card = deck.remove(deck.size()-1);
+        add_card_to_count(first_card);
+        player_hands.get(0).add(first_card);
+        int second_card = deck.remove(deck.size()-1);
+        add_card_to_count(second_card);
+        dealer_hand.add(second_card);
+        int third_card = deck.remove(deck.size()-1);
+        add_card_to_count(third_card);
+        player_hands.get(0).add(third_card);
+        int fourth_card = deck.remove(deck.size()-1);
+        add_card_to_count(fourth_card);
+        dealer_hand.add(fourth_card);
+    }
+
+    /**
+     * given a card, update the count
+     */
+    private void add_card_to_count(int card){
+        if (card == 10 || card == 1){
+            running_count--;
+            return;
+        }
+        if (card < 7){
+            running_count++;
+            return;
+        }
+    }
+
+
+    /**
+     * Given the dealer and player hands, plays the game optimally
+     * Recursively iterates until an end condition is reached (stand, double, bust or blackjack)
+     * Keeps track of running count, and constantly makes best decision as count changes
+     */
+    public boolean make_player_decision() {
+        // Make player decision
+        for(int i = 0; i < 4; i++){
+            hand = player_hands.get(i);
+            if (hand.isEmpty()) {break;}
+            play_hand(hand);
+        }
+
+        // Increment the count if hit
+    }
+
+    /**
+     * plays a single hand. If necessary, will add a player_hand and deal out the extra cards.
+     */
+    public void play_hand(List hand){
+        //play the hand
+        boolean keep_playing = true;
+        while(keep_playing){
+            // HANDLE SURRENDER
+            
+            // HANDLE ALL SPLITS
+            if(should_split()){
+                split(hand);
+                continue;
+            }
+
+            // HANDLE ALL DOUBLES
+
+            // HANDLE REMAINING SOFT TOTALS
+
+            // HANDLE REMAINING HARD TOTALS
+        }
+    }
+
+    /**
+     * Returns true if the player should split, false otherwise
+     * @param hand
+     * @return
+     */
+    private boolean should_split(List hand){
+        if (hand.size() != 2) {return false;}
+
+        int carda = hand.get(0);
+        int cardb = hand.get(1);
+        int dealer = dealer_hand.get(0);
+        int count = calculate_true_count();
+
+        if(carda != cardb) {return false;}
+        if(carda == 1) {return true;}
+        if(carda == 2) {
+            return 1 < dealer && dealer < 8;
+        }
+        if(carda == 3) {
+            return 1 < dealer && dealer < 8;
+        }
+        if(carda == 4) {
+            return dealer == 5 || dealer == 6;
+        }
+        if(carda == 5) {return false;}
+        if(carda == 6) {
+            //split 6 into 6 and lower
+            //stand 12 into 3 at TC 2
+            //stand 12 into 2 at TC 3
+            if (count >= 2.0 && dealer == 3){
+                return false;
+            }
+            if (count >= 3.0 && dealer == 2){
+                return false;
+            }
+            return 1 < dealer && dealer < 7;
+        }
+        if(carda == 7) {
+            return 1 < dealer && dealer < 8;
+        }
+        if(carda == 8) {return true;}
+        if(carda == 9) {
+            return dealer != 7 || dealer != 10 || dealer != 1;
+        }
+        if(carda == 10) {return false;}
+    }
+
+    /**
+     * Splits the hand into two hands if allowed (max 3 splits)
+     * @param hand
+     */
+    private void split(List hand){
+        //split if necessary
+        for(int i = 0; i < 4; i++){
+            if (player_hands.get(i).isEmpty()){
+                // remove second card from first hand
+                int split_card = hand.remove(1);
+                // add second card to second hand
+                player_hands.get(i).add(split_card);
+                // get a card from the deck to add to the first hand
+                int first_card = deck.remove(deck.size()-1);
+                add_card_to_count(first_card);
+                hand.add(first_card);
+                // get a card from the deck to add to the second hand
+                int second_card = deck.remove(deck.size()-1);
+                add_card_to_count(second_card);
+                player_hands.get(i).add(second_card);
+
+                return;
+            }
+        }
     }
 
     /**
      * Resets the table status.
-     * Convenience method to avoid creating new table instances for every loop in app.java
+     * Convenience method to avoid creating new table instances for every loop in App.java
      */
     public void reset(){
         bankroll = original_bankroll;
@@ -94,8 +272,53 @@ public class Table {
         running_count = 0;
         deck.clear();
         dealer_hand.clear();
-        player_hand.clear();
+        for (int i = 0; i < 4; i++){
+            player_hands.get(i).clear();
+        }
 
         shuffle();
     }
+
+    /**
+     * Generates a Stats object from the table state
+     * @return
+     */
+    public Stats get_stats(){
+        return stats = new Stats()
+
+    }
+
+    /**
+     * Plays the game at this table for the given number of hours
+     * or until bankroll reaches 0.
+     *
+     * @param target_hours
+     * @param target_profit
+     *
+     * Returns:
+     *  stats (Stats)
+     *
+     */
+    public Stats play_target_hours(double target_hours){
+        while(hours_played < target_hours && bankroll > 0.0){
+            //THE BIG LOOP
+            if (deck_needs_shuffle()){
+                shuffle();
+            }
+
+            deal_cards();
+
+            make_player_decision();
+
+            make_dealer_decision(deck, dealer_hand, running_count);
+
+            pay_out();
+        }
+        return Table.get_stats()
+    }
+}
+
+
+private class Stats(){
+
 }
