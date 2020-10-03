1966d483ae22bc2d0e7fa17ae537459f6fb7fab7
Andrew Gallup <agallup@Andrews-MBP.hsd1.ma.comcast.net>
2020-10-03 15:14:28 -0400

    first commit, adding table classes

---
+++ src/app.java
@@ -0,0 +1,9 @@
+/**
+Oct 3, 2020
+
+Betspread management software
+ Takes command line args and runs simulated blackjack games to determine risk of ruin, expected value, etc...
+ for the given input args.
+
+**/
+
